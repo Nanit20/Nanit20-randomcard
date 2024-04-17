69d965888e19740f0821fa521f06f3dd752c4a10
Nanit20 <55482552+Nanit20@users.noreply.github.com>
2024-04-17 15:24:32 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "&#9819;",
+    "&#9818;"
+  ];
+  let suit = {
+    hearts: "♥",
+    diamonds: "♦",
+    clubs: "♣",
+    spades: "♠"
+  };
+
+  let cardChosen = Math.floor(Math.random() * card.length);
+  let suitChosen = Math.floor(Math.random() * Object.keys(suit).length);
+
+  document.getElementById("card-number").innerHTML = card[cardChosen];
+  document.getElementById("card-top").innerHTML = Object.values(suit)[
+    suitChosen
+  ];
+  document.getElementById("card-bottom").innerHTML = Object.values(suit)[
+    suitChosen
+  ];
+
+  document
+    .getElementById("poker-card")
+    .classList.add(Object.keys(suit)[suitChosen]);
 };
